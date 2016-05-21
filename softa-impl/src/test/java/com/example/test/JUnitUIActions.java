992208a16810673402d990d80306acd05e6b764d
Yairo Mellado <jesus.mellado@softtek.com>
2016-05-21 13:29:52 -0700

    Impl Update

--- softa-impl/src/test/java/com/example/test/JUnitUIActions.java
+++ softa-impl/src/test/java/com/example/test/JUnitUIActions.java
@@ -1,5 +1,7 @@
 package com.example.test;
 
+import org.junit.Assert;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -38,16 +40,24 @@ public class JUnitUIActions extends AbstractSteps{
 	@Test
 	public void testUiActions() throws Exception{
 		//Testing actions: Overloading
+		assertTrue(UIActions.ElementNotExist("//input[@name='firstName']", null));
+		Assert.assertTrue(UIActions.GetValue("//input[@name='login']", null).getObjectResult().toString().equalsIgnoreCase("login"));
+		Assert.assertTrue(UIActions.GetText("//a[contains(text(), 'REGISTER')]", null).getObjectResult().toString().equalsIgnoreCase("register"));
+		assertTrue(UIActions.ElementIsEnabled("//a[contains(text(), 'REGISTER')]", null));
+		assertTrue(UIActions.ElementIsTypeOf("//a[contains(text(), 'REGISTER')]", null, "a"));
 		assertTrue(UIActions.ClickOnElement("//a[contains(text(), 'REGISTER')]", null));
 		assertTrue(UIActions.TypeTextOn("//input[@name='firstName']", null, "test"));
 		assertTrue(UIActions.TypeTextOn("//input[@name='address1']", null, "test"));
+		assertTrue(UIActions.MoveFocusTo("//input[@name='address1']", null));
+		assertTrue(UIActions.ElementHasFocus("//input[@name='address1']", null));
 		assertTrue(UIActions.VerifyMaxLengthText("//input[@name='address1']", null, 60));
 		assertTrue(UIActions.MoveMouseOverElement("//a[contains(text(), 'Flights')]", null));
 		assertTrue(UIActions.SelectValueFromDropdownElement("//select[@name='country']", null, "47"));
+		Assert.assertTrue(UIActions.GetSelectedValue("//select[@name='country']", null).getObjectResult().toString().equalsIgnoreCase("CROATIA"));
 		assertTrue(UIActions.TypeTextOn("//input[@name='email']", null, "test123"));
 		assertTrue(UIActions.TypeTextOn("//input[@name='password']", null, "test123"));
 		assertTrue(UIActions.TypeTextOn("//input[@name='confirmPassword']", null, "test123"));
-		assertTrue(UIActions.ClickOnElement("//input[@name='register']", null));
-		assertTrue(UIActions.ElementHasText("//div[contains(@class, 'footer')]", null, " © 2005, Mercury Interactive (v. 011003-1.01-058) "));
+		assertTrue(UIActions.ElementHasText("//div[contains(@class, 'footer')]", null, "© 2005, Mercury Interactive (v. 011003-1.01-058)"));
+		assertTrue(UIActions.ElementContainsText("//div[contains(@class, 'footer')]", null, "Mercury Interactive"));
 	}
 }
