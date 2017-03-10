80b7befefdf82a887feac7934389040dff37e908
luis.gonzalezc <alberto.gon@gmail.com>
2017-03-10 14:42:13 -0800

    update to test

--- softa-impl/src/test/java/com/org/test/CucumberTest.java
+++ softa-impl/src/test/java/com/org/test/CucumberTest.java
@@ -10,6 +10,6 @@ import cucumber.api.junit.Cucumber;
 		features = { "src/test/resources/feature/login-test.feature" },
 		glue = { "com.softtek.automation.steps.cucumber","com.org.test.steps" },
 		format = { "pretty", "html:cucumber-html-reports", "json:cucumber-html-reports/cucumber.json" },
-		tags = { "@test", "~@ignore" })
+		tags = { "@test", "~@ignore1" })
 public class CucumberTest {
 }
