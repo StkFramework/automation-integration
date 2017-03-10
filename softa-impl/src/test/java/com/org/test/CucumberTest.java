494b36277aaf134b086c8851d7b9cb09fcfa1263
edy.espinoza <edy.espinoza@LSTKEN100436.softtekclients.com>
2017-03-10 15:13:07 -0800

    new changes removed "1" from @ignore1

--- softa-impl/src/test/java/com/org/test/CucumberTest.java
+++ softa-impl/src/test/java/com/org/test/CucumberTest.java
@@ -4,12 +4,12 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-
+//this is a comment
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = { "src/test/resources/feature/login-test.feature" },
 		glue = { "com.softtek.automation.steps.cucumber","com.org.test.steps" },
 		format = { "pretty", "html:cucumber-html-reports", "json:cucumber-html-reports/cucumber.json" },
-		tags = { "@test", "~@ignore1" })
+		tags = { "@test", "~@ignore" })
 public class CucumberTest {
 }
